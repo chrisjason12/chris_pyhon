99998a25bc6914cf2cc68f0211e5f9cba0a4f8f6
chrisjason12 <167291155+chrisjason12@users.noreply.github.com>
2024-05-30 13:16:15 +0530

    Add files via upload

---
+++ program.sql
@@ -0,0 +1,25 @@
+select * from employee
+
+select min("salaey") 
+from employee
+
+select max("salary") from employee
+where dept = 'it_programmer';
+select count( distinct "dept")
+from employee
+
+select max(salaey)
+from employee
+where dept = 'it_programmer';
+
+select avg(salaey)
+from employee
+where dept_id = '90';
+
+select sum("salaey")
+from employee
+
+select "dept" from employee
+
+
+
